72099d3a418d24a64035e2c9e9100a61603a5683
madis <madis.pihlasalu@gmail.com>
2023-12-28 11:57:47 +0200

    add domain, dto, interface

---
+++ Cars.Core/ServiceInterface/ICarServices.cs
@@ -0,0 +1,19 @@
+﻿using Cars.Core.Domain;
+using Cars.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Cars.Core.ServiceInterface
+{
+    public interface ICarServices
+    {
+        Task<Car> Create(CarDto dto);
+        Task<Car> DetailsAsync(Guid id);
+        Task<Car> Delete(Guid id);
+        Task<Car> Update(CarDto dto);
+    }
+}

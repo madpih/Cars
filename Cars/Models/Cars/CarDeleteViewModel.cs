58969749e721a508fa52ff7a15f63cc79ca9bf9b
madis <madis.pihlasalu@gmail.com>
2023-12-29 12:27:16 +0200

    add viewmodels

---
+++ Cars/Models/Cars/CarDeleteViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Cars.Models.Cars
+{
+    public class CarDeleteViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Make { get; set; }
+        public DateTime InitialReg { get; set; }
+        public int Engine { get; set; }
+        public string Fuel { get; set; }
+        public int Mileage { get; set; }
+        public string Transmission { get; set; }
+
+        public string Color { get; set; }
+        public float Price { get; set; }
+
+        //public List<IFormFile> Files { get; set; }
+        //public IEnumerable<FileToDatabaseDto> Image { get; set; }
+        //    = new List<FileToDatabaseDto>();
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Cars/Models/Cars/CarIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Cars.Models.Cars
+{
+    public class CarIndexViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Make { get; set; } 
+        
+        public string Color { get; set; }
+        public string Transmission { get; set; }
+        public int Mileage { get; set; }
+       
+        public float Price { get; set; }
+
+        //public List<IFormFile> Files { get; set; }
+        //public IEnumerable<FileToDatabaseDto> Image { get; set; }
+        //    = new List<FileToDatabaseDto>();
+
+        
+    }
+}
